5a0e20219f26b01b3c1e46678c2500bef09f1633
Stephen Penn <stephepenn@qasymphony.com>
2018-03-30 13:57:09 -0400

    Implemented And statements in Amazon Authentication Feature

--- src/test/java/com/glue/Amazon_Authentication_Feature.java
+++ src/test/java/com/glue/Amazon_Authentication_Feature.java
@@ -3,6 +3,7 @@ package com.glue;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import cucumber.api.java.en.And;
 import org.junit.Assert;
 
 //  Scenario: AmazonLoginToOrder
@@ -23,7 +24,14 @@ public class Amazon_Authentication_Feature {
     public void I_put_in_my_credentials () throws Throwable {
         Assert.assertEquals(true, true);
     }
-
+    @And("^click Login$")
+    public void click_login () throws Throwable {
+        Assert.assertEquals(true, true);
+    }
+    @And("^go to the search field$")
+    public void i_go_to_the_search_field () throws Throwable {
+        Assert.assertEquals(true, true);
+    }
     @Then("^I should be able to find my item to order$")
     public void i_should_be_able_to_find_my_item_to_order () throws Throwable {
         Assert.assertEquals(true, true);
