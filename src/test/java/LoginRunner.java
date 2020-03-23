63203adf0c144fd664496615dd5f95f4a2f0df52
stepe <stepe@TRI-NB1078.tricentis.com>
2020-03-22 20:13:18 -0400

    some new updates

---
+++ src/test/java/LoginRunner.java
@@ -0,0 +1,16 @@
+
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features="projectfeature", 
+		glue="stepDefinitions",
+		tags =  "@endtoend" 
+		)
+public class LoginRunner {
+
+}
